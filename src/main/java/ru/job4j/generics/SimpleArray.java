3f257e247adc80b6773fd02d412158bd46a8cbb2
asidelnikov <a@a.ru>
2021-06-14 17:00:16 +0500

    5.2.1. Реализовать SimpleArray<T> [#156]

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -1,9 +1,10 @@
 package ru.job4j.generics;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 public class SimpleArray<T> implements Iterable<T> {
-    private Object[] array;
+    private final Object[] array;
     private int size = 0;
 
     public SimpleArray(int size) {
@@ -15,12 +16,35 @@ public class SimpleArray<T> implements Iterable<T> {
     }
 
     public T get(int index) {
+        Objects.checkIndex(index, size);
         return (T) array[index];
 
     }
 
+    public void set(int index, T model) {
+        Objects.checkIndex(index, size);
+        array[index] = model;
+    }
+
+    public void remove(int index) {
+        Objects.checkIndex(index, size);
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        array[size - 1] = null;
+        size--;
+    }
+
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new Iterator<T>() {
+            int point = 0;
+            @Override
+            public boolean hasNext() {
+                return point < array.length;
+            }
+            @Override
+            public T next() {
+                return (T)array[point++];
+            }
+        };
     }
 }
--- src/test/java/ru/job4j/generics/SimpleArrayTest.java
+++ src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -1,77 +1,67 @@
-package ru.job4j.generic;
+package ru.job4j.generics;
 
-import org.junit.Before;
 import org.junit.Test;
-import ru.job4j.generics.SimpleArray;
-
 import java.util.Iterator;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class SimpleArrayTest {
     private SimpleArray<Integer> simpleArray;
 
-    @Before
-    public void init() {
+    @Test
+    public void whenAddModel() {
         simpleArray = new SimpleArray<>(3);
         simpleArray.add(1);
         simpleArray.add(2);
         simpleArray.add(3);
-    }
 
-    @Test
-    public void whenAddModel() {
         assertThat(simpleArray.get(0), is(1));
         assertThat(simpleArray.get(1), is(2));
         assertThat(simpleArray.get(2), is(3));
     }
 
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void whenAddModel1() {
-        assertThat(simpleArray.get(3), is(4));
-    }
-
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void whenAddNewModelMoreArrayLength() {
-        simpleArray.add(4);
-    }
-/*
     @Test
     public void whenRemoveSecondModel() {
+        simpleArray = new SimpleArray<>(3);
+        simpleArray.add(1);
+        simpleArray.add(2);
+        simpleArray.add(3);
         simpleArray.remove(1);
         assertThat(simpleArray.get(0), is(1));
         assertThat(simpleArray.get(1), is(3));
-        assertNull(simpleArray.get(2));
     }
 
     @Test
     public void whenRemoveLastModel() {
+        simpleArray = new SimpleArray<>(3);
+        simpleArray.add(1);
+        simpleArray.add(2);
+        simpleArray.add(3);
         simpleArray.remove(2);
         assertThat(simpleArray.get(0), is(1));
         assertThat(simpleArray.get(1), is(2));
-        assertNull(simpleArray.get(2));
-    }
-
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void whenRemoveIndexGreatThanArrayLength() {
-        simpleArray.remove(10);
     }
 
     @Test
-    public void whenSetNewModelIsInArray() {
-        simpleArray.set(2, 100);
-        assertThat(simpleArray.get(2), is(100));
-    }
-
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void whenSetNewModelNotInArray() {
-        simpleArray.set(100, 1000);
+    public void whenSetModel() {
+        simpleArray = new SimpleArray<>(3);
+        simpleArray.add(1);
+        simpleArray.add(2);
+        simpleArray.add(3);
+        assertThat(simpleArray.get(0), is(1));
+        assertThat(simpleArray.get(1), is(2));
+        assertThat(simpleArray.get(2), is(3));
+        simpleArray.set(2, 4);
+        assertThat(simpleArray.get(2), is(4));
     }
 
     @Test
-    public void whenIteratorWorksCorrectly() {
-        Iterator simpleArrayIt = simpleArray.iterator();
+    public void whenIteratorOk() {
+        simpleArray = new SimpleArray<>(3);
+        simpleArray.add(1);
+        simpleArray.add(2);
+        simpleArray.add(3);
+        Iterator<Integer> simpleArrayIt = simpleArray.iterator();
         assertThat(simpleArrayIt.hasNext(), is(true));
         assertThat(simpleArrayIt.next(), is(1));
         assertThat(simpleArrayIt.hasNext(), is(true));
@@ -79,5 +69,5 @@ public class SimpleArrayTest {
         assertThat(simpleArrayIt.hasNext(), is(true));
         assertThat(simpleArrayIt.next(), is(3));
         assertThat(simpleArrayIt.hasNext(), is(false));
-    }*/
+    }
 }
