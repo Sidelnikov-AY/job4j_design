011f437a8b3f763e46f5738616cf6e8e71e61c21
asidelnikov <a@a.ru>
2021-06-15 17:03:38 +0500

    5.2.2. Реализовать Store<T extends Base> [#157]

---
+++ src/main/java/ru/job4j/generics/MemStore.java
@@ -0,0 +1,39 @@
+package ru.job4j.generics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class MemStore<T extends Base> implements Store<T> {
+
+    private final Map<String, T> mem = new HashMap<>();
+
+    @Override
+    public void add(T model) {
+        mem.put(model.getId(), model);
+    }
+
+    @Override
+    public boolean replace(String id, T model) {
+        boolean contains = mem.containsKey(id);
+        if(contains) {
+            mem.replace(id, model);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        boolean contains = mem.containsKey(id);
+        if (contains) {
+            mem.remove(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public T findById(String id) {
+        return mem.get(id);
+    }
+}
---
+++ src/test/java/ru/job4j/generics/MemStoreTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.generics;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class MemStoreTest {
+    MemStore<User> memStore = new MemStore<>();
+    User user1 = new User("1", "test1");
+    User user2 = new User("2", "test2");
+    User user3 = new User("3", "test3");
+    User user4 = new User("4", "test4");
+
+    @Before
+    public void init() {
+        memStore.add(user1);
+        memStore.add(user2);
+        memStore.add(user3);
+        memStore.add(user4);
+    }
+
+    @Test
+    public void WhenAddModel() {
+        assertEquals(user1, memStore.findById("1"));
+        assertEquals(user2, memStore.findById("2"));
+        assertEquals(user3, memStore.findById("3"));
+        assertEquals(user4, memStore.findById("4"));
+    }
+
+    @Test
+    public void WhenReplace() {
+        memStore.replace("3", user4);
+        assertEquals(user4, memStore.findById("4"));
+        assertEquals(user4, memStore.findById("4"));
+    }
+
+    @Test
+    public void WhenDelete() {
+        assertTrue(memStore.delete("4"));
+    }
+
+    @Test
+    public void WhenFindById() {
+        assertEquals(user2, memStore.findById("2"));
+    }
+
+    @Test
+    public void WhenFindNull() {
+        assertNull(memStore.findById("5"));
+    }
+
+}
