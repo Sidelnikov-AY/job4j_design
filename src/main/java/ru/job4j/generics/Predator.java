d915f6cd5d54864c381f33dd8fcbc967c602d375
asidelnikov <a@a.ru>
2021-06-20 17:28:45 +0500

    2. Создать контейнер на базе связанного списка [#159 #168987]

--- src/main/java/ru/job4j/generics/Predator.java
+++ src/main/java/ru/job4j/generics/Predator.java
@@ -1,7 +1,7 @@
 package ru.job4j.generics;
 
-public class Predator extends Animal{
-    public Predator() {
+public class Predator extends Animal {
+    public Predator()  {
         super();
     }
 }
