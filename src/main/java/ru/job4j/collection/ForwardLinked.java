128115f9a34a016c90ae98c2bc0c47ca7b2a802d
asidelnikov <a@a.ru>
2021-06-22 11:13:46 +0500

    6. Перевернуть связанный список [#161]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -26,6 +26,22 @@ public class ForwardLinked<T> implements Iterable<T> {
         size++;
     }
 
+    public boolean revert() {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        Node<T> prev = null;
+        Node<T> current = head;
+        while (current != null) {
+            Node<T> next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return true;
+    }
+
     public void addFirst(T value) {
         Node<T> node = new Node<T>(value, null);
         if (head == null) {
