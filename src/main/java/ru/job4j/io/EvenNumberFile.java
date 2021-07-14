3669f27d333a7344f6db9d9bf1d38d34d7522dfa
sid <1@1.ru>
2021-07-14 22:05:02 +0500

    0.2. FileInputStream [#4898]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+           }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                int dig =Integer.parseInt(line);
+                String answer = (dig % 2 == 0) ? dig + " - " + "четное" : dig + " - " + "нечетное";
+                System.out.println(answer);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
