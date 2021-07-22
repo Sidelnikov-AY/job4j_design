2fb8e1db610b8fc2cec5efce3e6c6416a5d77f19
sid <1@1.ru>
2021-07-22 22:58:12 +0500

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,56 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Analizy {
+    public Analizy(){
+
+    };
+    public void unavailable(String source, String target) {
+        List<String> time = new ArrayList<>();
+        String start = null;
+        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
+            String line = read.readLine();
+            while (line != null) {
+                String[] listFromLine = line.split(" ");
+                int status = Integer.parseInt(listFromLine[0]);
+                if (status >= 400 && start == null) {
+                    start = listFromLine[1];
+                }
+                if (status < 400 && start != null) {
+                    time.add(start + ";" + listFromLine[1] + ";");
+                    start = null;
+
+                }
+                line = read.readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(time.size() == 0) {
+            return;
+        }
+        save(time, target);
+
+    }
+
+    public static void save(List<String> list, String target) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(target)
+                ))) {
+            for (String e: list) {
+                out.println(e);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analizy analysis = new Analizy();
+        analysis.unavailable("analysis.csv", "out.csv");
+    }
+}
