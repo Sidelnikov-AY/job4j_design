9641bd9744a910b2a2c1a6c2b88fcb8b460f661c
sid <1@1.ru>
2021-07-27 22:22:27 +0500

    2. Анализ доступности сервера. [#859 #180633]

---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+import java.util.List;
+
+public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void unaviable() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.printf("200 10:56:01%n500 10:57:01%n400 10:58:01%n200 10:59:01%n500 11:01:02%n200 11:02:02");
+        }
+
+        Analizy analysis = new Analizy();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01;10:59:01;" +
+                "11:01:02;11:02:02;"));
+    }
+}
