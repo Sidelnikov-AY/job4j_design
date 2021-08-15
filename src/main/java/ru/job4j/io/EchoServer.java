3e738995e3d54b6295783e4fa5f88e671b5fc8a9
asidelnikov <a@a.ru>
2021-08-15 18:09:47 +0500

    1. Бот [#7921]v

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,19 +6,30 @@ import java.net.Socket;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
+        final String exit = "Exit";
+        final String hello = "Hello";
+        final String what = "What";
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
-                        if (str.contains("Bye")) {
+                    String str = in.readLine();
+                    System.out.println(str);
+                        if (str.contains(hello)) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write("Hello".getBytes());
+                            System.out.println(str);
+                        } else if ((str.contains(exit))) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            System.out.println(str);
                             server.close();
+                        } else {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write("What".getBytes());
+                            System.out.println(str);
                         }
-                    }
                     out.flush();
                 }
             }
