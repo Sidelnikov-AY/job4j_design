e2bb430571a4fd71a4b4762e496f792ce82477f0
asidelnikov <a@a.ru>
2021-10-24 16:46:56 +0500

    3. Фильтры [#1732 #217192]

--- data/role_scheme/product.sql
+++ data/role_scheme/product.sql
@@ -30,13 +30,13 @@ insert into type(name) values("МОЛОКО");
 insert into type(name) values("МОРОЖЕНОЕ");
 
 --1. Написать запрос получение всех продуктов с типом "СЫР"
-select * from product where type_id =1;
+select * from product join type on type.name = 'СЫР';
 
 --2. Написать запрос получения всех продуктов, у кого в имени есть слово "мороженое"
 select * from product where name like 'мороженое%';
 
 --3. Написать запрос, который выводит все продукты, срок годности которых уже истек
-select * from product where expired_date < '2021-10-17';
+select * from product where expired_date < current_date;
 
 --4. Написать запрос, который выводит самый дорогой продукт.
 select name, max(price) from product
@@ -45,16 +45,16 @@ order by max(price) desc
 limit 1;
 
 --5. Написать запрос, который выводит для каждого типа количество продуктов к нему принадлежащих. В виде имя_типа, количество
-select type.name, count(product.type_id) from product join type on type.id = product.type_id
+select type.name, count(p.type_id) from product as p join type on type.id = p.type_id
 group by type.name;
 
 --6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
-select * from product where type_id =1 or type_id =2;
+select * from product join type on type.name = 'СЫР' or type.name = 'МОЛОКО';
 
 --7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук. Под количеством подразумевается количество продуктов определенного типа. Например, если есть тип "СЫР" и продукты "Сыр плавленный" и "Сыр моцарелла", которые ему принадлежат, то количество продуктов типа "СЫР" будет 2.
-select type.name, count(product.type_id) from product join type on type.id = product.type_id
+select type.name, count(p.type_id) from product as p join type on type.id = p.type_id
 group by type.name
-having count(product.type_id) < 10;
+having count(p.type_id) < 10;
 
 --8. Вывести все продукты и их тип.
 select * from product join type on product.type_id = type.id;
